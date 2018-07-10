aa7af9dc989a5d72492f2fbbbfb2efa7f3874860
aungaungsvr3 <31975186+aungaungsvr3@users.noreply.github.com>
2018-07-10 20:56:43 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -42,8 +42,8 @@ $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['extension'] = 'mysqli';
 $cfg['Servers'][$i]['auth_type'] = 'config';
 $cfg['Servers'][$i]['user'] = 'root';
-$cfg['Servers'][$i]['password'] = '';
-$cfg['Servers'][$i]['AllowNoPassword'] = true;
+$cfg['Servers'][$i]['password'] = 'root123!@#';
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
 
 /**
  * phpMyAdmin configuration storage settings.
