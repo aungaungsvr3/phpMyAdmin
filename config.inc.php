7f1b3dbbdcfd3b9ea462999ad5c4d9bb0821ba2f
aungaungsvr3 <31975186+aungaungsvr3@users.noreply.github.com>
2018-07-10 20:42:43 +0800

    modify the phpmyadmin user name and password.

--- config.inc.php
+++ config.inc.php
@@ -31,6 +31,8 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = 'mysql';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['password'] = 'root';
 
 /**
  * phpMyAdmin configuration storage settings.
