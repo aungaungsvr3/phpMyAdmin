5164ab0597bf77d6dc99b7756fa9eca3d7597aa9
aungaungsvr3 <31975186+aungaungsvr3@users.noreply.github.com>
2018-07-13 22:18:40 +0800

    encrypt key for cookies

--- config.inc.php
+++ config.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'openshiftsphpMyAdmin!@#AMM1234./'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
